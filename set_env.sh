10a85273699799fe2ec9aebe6e8396ddefddc139
NASR ALLAH Mounir <nasrallah.mounir@gmail.com>
2015-08-20 19:40:30 +0100

    Version 2

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -fs "$DIR"/bashrc ~/.bashrc
+ln -fs "$DIR"/bashrc ~/.bash_profile
 ln -fs "$DIR"/bashrc ~/.profile
 ln -fs "$DIR"/emacs ~/.emacs
