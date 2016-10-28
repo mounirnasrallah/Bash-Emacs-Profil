3b655e9787234a0d70ac28ce89e3007e05a6891c
Mounir NASR ALLAH <mounir.nasrallah@centralesupelec.fr>
2016-10-28 16:10:21 +0200

    tmp

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=`pwd`
 
-ln -fs "$DIR"/bashrc ~/.bashrc
-ln -fs "$DIR"/bashrc ~/.bash_profile
-ln -fs "$DIR"/bashrc ~/.profile
-ln -fs "$DIR"/emacs ~/.emacs
+ln -fs "${DIR}"/bashrc ~/.bashrc
+ln -fs "${DIR}"/bashrc ~/.bash_profile
+ln -fs "${DIR}"/bashrc ~/.profile
+ln -fs "${DIR}"/emacs ~/.emacs
